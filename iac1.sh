bc854c1deccd05269bbc0d412e24aef3438c6918
FillipeBraz <47422159+FillipeBraz@users.noreply.github.com>
2023-05-07 17:00:57 -0300

    Update iac1.sh

--- iac1.sh
+++ iac1.sh
@@ -2,10 +2,7 @@
 
 echo "Criando diretórios..."
 
-mkdir /publico
-mkdir /adm
-mkdir /ven
-mkdir /sec
+mkdir /publico /adm /ven /sec
 
 echo "Criando grupos de usuários..."
 
@@ -15,19 +12,19 @@ groupadd GRP_SEC
 
 echo "Criando usuários..."
 
-useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
-useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
-useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd carlos -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_ADM
 
-useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
-useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
-useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd debora -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_VEN
 
-useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
-useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
-useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd josefina -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_SEC
 
-echo "Especificando permissões dos diretórios...."
+echo "Especificando permissões dos diretórios..."
 
 chown root:GRP_ADM /adm
 chown root:GRP_VEN /ven
@@ -38,5 +35,5 @@ chmod 770 /ven
 chmod 770 /sec
 chmod 777 /publico
 
-echo "Fim....."
+echo "Finalizado"
 
